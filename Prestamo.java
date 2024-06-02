6d8e93c1cb4fd5b79dd6055a6548c3b4d906f2ba
Jose Velazquez <111530488+Cheche1233@users.noreply.github.com>
2024-06-02 14:47:29 -0700

    Create Prestamo.java

---
+++ Prestamo.java
@@ -0,0 +1,58 @@
+import java.util.Date;
+
+public class Prestamo {
+    private int id;
+    private Usuario usuario;
+    private Libro libro;
+    private Date fechaPrestamo;
+    private Date fechaDevolucion;
+    private boolean devuelto;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public Date getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(Date fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public Date getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(Date fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public boolean isDevuelto() {
+        return devuelto;
+    }
+
+    public void setDevuelto(boolean devuelto) {
+        this.devuelto = devuelto;
+    }
+}
