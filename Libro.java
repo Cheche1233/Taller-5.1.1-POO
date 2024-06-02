cb64b7bc093dabc6ff6826a8055d82261c156a65
Jose Velazquez <111530488+Cheche1233@users.noreply.github.com>
2024-06-02 14:46:43 -0700

    Create Libro.java

---
+++ Libro.java
@@ -0,0 +1,47 @@
+public class Libro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int copiasDisponibles;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getCopiasDisponibles() {
+        return copiasDisponibles;
+    }
+
+    public void setCopiasDisponibles(int copiasDisponibles) {
+        this.copiasDisponibles = copiasDisponibles;
+    }
+}
